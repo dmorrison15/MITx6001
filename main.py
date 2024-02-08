e101523aba9d99935188fc14474a3f1c47281f49
danimorrison15 <31575438-danimorrison15@users.noreply.replit.com>
2024-02-08 18:30:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+starting_balance = input('Give a balance amount: ')
+annual_interest_rate = input('Give an interst rate: ')
+monthly_payment_rate = input('Give a monthly payment rate: ')
+
+cur_balance = starting_balance
