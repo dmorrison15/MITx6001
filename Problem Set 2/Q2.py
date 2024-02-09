a924132dffb8144cc8c7f0e184e1fe2c587cd3f6
dmorrison15 <159485441+dmorrison15@users.noreply.github.com>
2024-02-09 20:01:50 +0000

    compare balance with monthly payment with total balance

--- Problem Set 2/Q2.py
+++ Problem Set 2/Q2.py
@@ -1,2 +1,13 @@
 balance = int(input('balance: '))
-annualInterestRate = int(input('annual interest rate: '))
+curBalance = 0
+annualInterestRate = float(input('annual interest rate: '))
+monthlyPayment = 10
+monthlyInterestRate = annualInterestRate / 12
+while True:
+  for i in range(12) :
+    unpaid_balance = balance - monthlyPayment
+    curBalance = round(unpaid_balance + (monthlyInterestRate * unpaid_balance),  12)
+  if curBalance >= balance :
+    break
+  monthlyPayment += 10
+print('Lowest Payment:', monthlyPayment)
