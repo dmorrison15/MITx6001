0de570c313fc742b17d929d57ec72b20e7d34b2b
Danielle <159485441+dmorrison15@users.noreply.github.com>
2024-02-12 20:39:17 +0000

    complete q2

---
+++ main.py
@@ -0,0 +1,1 @@
+import pset2.Q2
--- pset2/Q2.py
+++ pset2/Q2.py
@@ -1,13 +1,21 @@
-balance = int(input('balance: '))
-curBalance = balance
-annualInterestRate = float(input('annual interest rate: '))
+# CONSTANTS
+balance = 3329
+annualInterestRate = 0.2
 monthlyInterestRate = annualInterestRate / 12
+
+# Logic
 monthlyPayment = 10
+# infinite loop that will keep increasing monthly payment by 10 so long as balance isn't paid off
 while True:
+  #reset current balance to initial balance
+  curBalance = balance
+  # determine unpaid balance each month
   for i in range(12) :
     unpaid_balance = curBalance - monthlyPayment
     curBalance = round(unpaid_balance + (monthlyInterestRate * unpaid_balance),  2)
-  if curBalance <= balance :
+  
+  # if current balance is fully paid off then break out of infinite loop  
+  if curBalance <= 0 :
     break
   monthlyPayment += 10
 print('Lowest Payment:', monthlyPayment)
