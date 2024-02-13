374af015540831b07d16a2f21fda5d1e93fa4ea9
Danielle <danimorrison15@gmail.com>
2024-02-13 18:50:46 +0000

    complete q3

--- pset2/Q3.py
+++ pset2/Q3.py
@@ -1,24 +1,25 @@
-# Constants
-balance = 999999
-annualInterestRate = 0.18
+# CONSTANTS
+balance = 320000
+annualInterestRate = 0.20
 
+# The upper and lower bounds are the boundaries in which the answer will be searched for. The upper and lower bounds are each initialized with their highest or lowest possible values. 
 upperBound = (balance + balance * annualInterestRate) / 12
 lowerBound = balance / 12
+
 monthlyInterestRate = annualInterestRate / 12
 
-# This infinite while loop uses bisection search to find the monthly payment so long as the current balance after a year is not 0
+# This infinite while loop uses bisection search to find the monthly payment so long as the current balance is within a few of 0.
 while True :
   monthlyPayment = round((upperBound + lowerBound) / 2, 2)
-  print(monthlyPayment)
   curBalance = balance
   for i in range(12) :
     unpaid_balance = curBalance - monthlyPayment
     curBalance = round((unpaid_balance + monthlyInterestRate * unpaid_balance), 2)
+  if abs(curBalance) < 0.15 :
+    break
   if curBalance > 0 :
     lowerBound = monthlyPayment
   elif curBalance < 0 :
-    upperBound = monthlyPayment    
-  else :
-    break
+    upperBound = monthlyPayment
+  # The monthly payment has a margin of error of about 1 cent
 print('Lowest Payment:', monthlyPayment)
-# 29157.09
