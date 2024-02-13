18b49f5912450dfd4c90381c150ec938ddb01363
Danielle <danimorrison15@gmail.com>
2024-02-13 18:32:13 +0000

    start debugging while loop

--- pset2/Q3.py
+++ pset2/Q3.py
@@ -1,22 +1,24 @@
 # Constants
-balance = 320000
-annualInterestRate = 0.2
+balance = 999999
+annualInterestRate = 0.18
 
-upperBound = balance
-lowerBound = 0
+upperBound = (balance + balance * annualInterestRate) / 12
+lowerBound = balance / 12
 monthlyInterestRate = annualInterestRate / 12
 
 # This infinite while loop uses bisection search to find the monthly payment so long as the current balance after a year is not 0
 while True :
   monthlyPayment = round((upperBound + lowerBound) / 2, 2)
+  print(monthlyPayment)
   curBalance = balance
   for i in range(12) :
     unpaid_balance = curBalance - monthlyPayment
-    curBalance = round(unpaid_balance +(monthlyInterestRate * unpaid_balance),  2)
+    curBalance = round((unpaid_balance + monthlyInterestRate * unpaid_balance), 2)
   if curBalance > 0 :
     lowerBound = monthlyPayment
   elif curBalance < 0 :
-    upperBound = monthlyPayment
+    upperBound = monthlyPayment    
   else :
     break
 print('Lowest Payment:', monthlyPayment)
+# 29157.09
