876df7a487e8dbd07fc0df5cb3531336b2710cda
dmorrison15 <159485441+dmorrison15@users.noreply.github.com>
2024-02-28 21:49:12 +0000

    Complete playHand function

--- pset4/ps4a.py
+++ pset4/ps4a.py
@@ -234,34 +234,40 @@ def playHand(hand, wordList, n):
     """
     # BEGIN PSEUDOCODE <-- Remove this comment when you code this function; do your coding within the pseudocode (leaving those comments in-place!)
     # Keep track of the total score
-
+    total = 0
     # As long as there are still letters left in the hand:
-
+    while calculateHandlen(hand) > 0 :
         # Display the hand
-
+      print('Current hand: ', end = '')
+      displayHand(hand)
         # Ask user for input
-
+      word = input('Enter word, or a "." to indicate that you are finished: ')
         # If the input is a single period:
-
+      if word == '.' :
             # End the game (break out of the loop)
-
+          break
 
         # Otherwise (the input is not a single period):
-
+      while not isValidWord(word, hand, wordList) :
             # If the word is not valid:
 
                 # Reject invalid word (print a message followed by a blank line)
-
+        print("Invalid word, please try again. + \n")
             # Otherwise (the word is valid):
-
+      
                 # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
-
-                # Update the hand 
-
-
+      score = getWordScore(word, n)
+      total += score
+                 
+      print('"' + word + '"', 'earned', score, 'points. Total:', total)
+                # Update the hand
+      hand = updateHand(hand, word)
     # Game is over (user entered a '.' or ran out of letters), so tell user the total score
-
-
+    if calculateHandlen(hand) == 0 :
+      print('Ran out of letters. Total score:', total)
+    else :
+      print('Goodbye! Total score:', total)
+    
 #
 # Problem #5: Playing a game
 # 
